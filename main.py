4f9b157563fe27755695c5d89f9a7dd53eef7b6d
JulietaCaceres <julieta.agustina.caceres@gmail.com>
2022-04-06 14:04:47 -0300

    Adding type to arg

--- main.py
+++ main.py
@@ -2,13 +2,13 @@ from solucion_optima import *
 import argparse
 
 parser = argparse.ArgumentParser(description="Obtain the optimal solution")
-parser.add_argument("k", help="Size of the road", default=500)
+parser.add_argument("k", type=int, help="Size of the road", default=500)
 parser.add_argument("file", help="File with contracts", default="contratos.txt")
 
 
 if __name__ == "__main__":
     args = parser.parse_args()
-    print(best_contracts(int(args.k), args.file))
+    print(best_contracts(args.k, args.file))
 
 
 

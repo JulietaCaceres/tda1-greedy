bcdf2da0d5dba4face37818085df8ea35f4c2747
JulietaCaceres <julieta.agustina.caceres@gmail.com>
2022-05-11 22:13:34 -0300

    Fixing logic

--- solucion_optima.py
+++ solucion_optima.py
@@ -1,3 +1,10 @@
+from operator import le
+import re
+
+
+ERROR_MESSAGE = "No se puede cubrir la ruta"
+
+
 def process_antennas(file_name):
     available_antennas = []
 
@@ -10,34 +17,64 @@ def process_antennas(file_name):
 
     available_antennas.sort(key=lambda x: x[1])
 
+
     return available_antennas
 
 
 def route_is_covered(start, k):
     return start >= k
 
+def there_are_antennas(available_antennas):
+    return not len(available_antennas) == 0
+
+def can_cover_road_length(available_antennas, k):
+    return not max(available_antennas, key=lambda x: x[2])[2] < k
+
+
+def start_can_be_covered(available_antennas):
+    return not available_antennas[0][1] > 0
+
+
+def check_border_cases(available_antennas, k):
+    return there_are_antennas(available_antennas) and can_cover_road_length(available_antennas, k) and start_can_be_covered(available_antennas)
+
+
 
 def best_contracts(k, file_name):
     available_antennas = process_antennas(file_name)
 
-    if max(available_antennas, key=lambda x: x[2])[2] < k:
-        return "No se puede cubrir la ruta completamente"
+    valid = check_border_cases(available_antennas, k)
+    if not valid:
+        return ERROR_MESSAGE
 
     contracts = []
+    end_optimal_antenna = available_antennas[0][2]
     optimal_antenna = available_antennas[0]
     start = 0
 
     for i, antenna in enumerate(available_antennas[1:]):
         if route_is_covered(start, k):
-            continue
-        if antenna[1] <= start and optimal_antenna[2] <= antenna[2]:
+            return  [contract[0] for contract in contracts]
+        start_antenna = antenna[1]
+        end_antenna = antenna[2]
+        if start_antenna <= start and end_optimal_antenna <= end_antenna:
+            end_optimal_antenna = end_antenna
             optimal_antenna = antenna
-        elif antenna[1] > start:
-            start = optimal_antenna[2]
+        elif start_antenna > start:
+            start = end_optimal_antenna
             contracts.append(optimal_antenna)
+            if route_is_covered(start, k):
+                return  [contract[0] for contract in contracts]
+            if start_antenna <= start:
+                end_optimal_antenna = end_antenna
+                optimal_antenna = antenna
+            else:
+                return ERROR_MESSAGE
+    
+    last_antennna = available_antennas[-1]
+    if last_antennna[1] <= start:
+        contracts.append(last_antennna)
+    else:
+        return ERROR_MESSAGE
 
-            optimal_antenna = antenna
-            if i == len(available_antennas) - 2 and contracts[-1][2] < k:
-                contracts.append(antenna)
-
-    return [contract[0] for contract in contracts]
+    return  [contract[0] for contract in contracts]
